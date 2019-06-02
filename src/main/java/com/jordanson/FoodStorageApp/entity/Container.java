f8f43015582b66f818b9a7575665e5137104631c
jordanson <robertsonjordan18@gmail.com>
2019-06-01 20:24:43 -0400

    committing with finished entities

---
+++ src/main/java/com/jordanson/FoodStorageApp/entity/Container.java
@@ -0,0 +1,75 @@
+package com.jordanson.FoodStorageApp.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="container")
+public class Container {
+	
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	private long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@OneToMany(mappedBy="container", cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, orphanRemoval=true)
+	private List<Food> foodList;
+	
+	public Container(String name) {
+		this.name = name;
+	}
+	
+	public Container() {
+		
+	}
+	
+	public void addFood(Food food) {
+		if (foodList == null) {
+			foodList = new ArrayList<>();
+		}
+		
+		foodList.add(food);
+		food.setContainer(this);
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Food> getFoodList() {
+		return foodList;
+	}
+
+	public void setItems(List<Food> foodList) {
+		this.foodList = foodList;
+	}
+
+	@Override
+	public String toString() {
+		return "Container [id=" + id + ", name=" + name + ", foodList=" + foodList + "]";
+	}
+}
---
+++ src/main/java/com/jordanson/FoodStorageApp/entity/Food.java
@@ -0,0 +1,149 @@
+package com.jordanson.FoodStorageApp.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="food")
+public class Food {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="type")
+	private String type;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="date")
+	private LocalDate expiration;
+	
+	@Column(name="quantity")
+	private int quantity;
+	
+	@ManyToOne
+	@JoinColumn(name="container_id", foreignKey = @ForeignKey(name = "FK_FOOD"))
+	private Container container;
+	
+	private Food(FoodBuilder foodbuilder) {
+		this.name = foodbuilder.name;
+		this.type = foodbuilder.type;
+		this.description = foodbuilder.description;
+		this.expiration = foodbuilder.expiration;
+		this.quantity = foodbuilder.quantity;
+	}
+	
+	public Food() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getExpiration() {
+		return expiration;
+	}
+
+	public void setExpiration(LocalDate expiration) {
+		this.expiration = expiration;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Container getContainer() {
+		return container;
+	}
+
+	public void setContainer(Container container) {
+		this.container = container;
+	}
+
+	@Override
+	public String toString() {
+		return "Food [id=" + id + ", name=" + name + ", type=" + type + ", description=" + description + ", expiration="
+				+ expiration + ", quantity=" + quantity + ", container=" + container + "]";
+	}
+	
+	
+	public static class FoodBuilder {
+		private String name;	
+		private String type;
+		private String description;
+		private LocalDate expiration;
+		private int quantity;
+		
+		public FoodBuilder(String name, String type) {
+			this.name = name;
+			this.type = type;
+		}
+		
+		public FoodBuilder setDescription(String description) {
+			this.description = description;
+			return this;
+		}
+		
+		public FoodBuilder setExpiration(LocalDate expiration) {
+			this.expiration = expiration;
+			return this;
+		}
+		
+		public FoodBuilder setQuantity(int quantity) {
+			this.quantity = quantity;
+			return this;
+		}
+		
+		public Food buildFood(){
+			return new Food(this);
+		}
+	}
+	
+}
