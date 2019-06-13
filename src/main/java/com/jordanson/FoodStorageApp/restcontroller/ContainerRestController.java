524fbb121ca4a3dc6fb7aabbef86ce19db8b727b
jordanson <robertsonjordan18@gmail.com>
2019-06-13 01:44:21 -0400

    pushed finished containerrestcontroller again as the initial push didn't include all of the updates

--- src/main/java/com/jordanson/FoodStorageApp/restcontroller/ContainerRestController.java
+++ src/main/java/com/jordanson/FoodStorageApp/restcontroller/ContainerRestController.java
@@ -1,5 +1,75 @@
 package com.jordanson.FoodStorageApp.restcontroller;
 
-public class ContainerRestController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.jordanson.FoodStorageApp.dao.ContainerDAO;
+import com.jordanson.FoodStorageApp.entity.Container;
+
+
+
+public class ContainerRestController {
+	private ContainerDAO containerDAO;
+	
+	public ContainerRestController(ContainerDAO containerDAO) {
+		this.containerDAO = containerDAO;
+	}
+	
+	@GetMapping("/containers")
+	public List<Container> getContainers() {
+		return containerDAO.findAll();
+	}
+
+	@GetMapping("/containers/{containerId}")
+	public Container getContainerByID(@PathVariable int containerId) {
+		Container container = containerDAO.findById(containerId);
+		
+		if (container == null) {
+			throw new RuntimeException("Container id not found " + containerId);
+		}
+		
+		return container;
+	}
+	
+	//why return here?
+	@PostMapping("/containers")
+	public Container addContainer(@RequestBody Container container) {
+		//just in case they pass an id in JSON, set id to zero 
+		//this is to force a save of new item, instead of update 
+		container.setId(0);
+		
+		containerDAO.save(container);
+		
+		return container;
+	}
+	
+	//why return here as well?
+	@PutMapping("/containers")
+	public Container updateContainer(@RequestBody Container container) {
+		containerDAO.save(container);
+		
+		return container;
+		//despite postman returning the updated data, i could not get mysql to reflect the changes
+		//until i added @Transactional to the "save" method in the Service layer. 
+		//i did not have any issues adding an employee prior to adding @transactional, however
+	}
+	
+	@DeleteMapping("/containers/{containerId}")
+	public String deleteContainer(@PathVariable int containerId) {
+		Container container = containerDAO.findById(containerId);
+		
+		if (container == null) {
+			throw new RuntimeException("The employee is not found - " + containerId);
+		}
+		
+		containerDAO.deleteById(containerId);
+		
+		return "Deleted employee id - " + containerId;
+	}
 }
